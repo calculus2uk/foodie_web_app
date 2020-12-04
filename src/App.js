07b8446205ff79ab3da18f515d8b5bf97e4fedfd
Sir Calculus <calculus2uk@gmail.com>
2020-12-04 19:45:37 +0200

    Added the react boiler plate files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Foodie</header>
+			<h1>Hello World Of Foodie</h1>
+		</div>
+	);
+}
+
+export default App;
