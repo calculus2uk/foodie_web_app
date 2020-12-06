47224de9ed3aececcf6c00fdd8c3ed771cb178fa
Sir Calculus <calculus2uk@gmail.com>
2020-12-06 04:42:02 +0200

    Added the layout for the first page when app loads

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+//CSS
 import "./App.css";
+
+//SVG
+import { ReactComponent as Logo } from "./images/logo.svg";
+import { ReactComponent as OK } from "./images/bev.svg";
+
+//IMAGES
+
+//Components
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">Foodie</header>
-			<h1>Hello World Of Foodie</h1>
+			<Logo className="logo" />
+			<h1>
+				<span>Food for </span>
+				<span>Everyone</span>
+			</h1>
+			<OK style={{ width: "100vw", height: "50vh" }} />
+			<button>Get Started</button>
 		</div>
 	);
 }
